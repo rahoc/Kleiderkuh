9cfe42ade1991e57b0ed245b2217e42e048b239a
rahoc <rahoc@aal-web.de>
2013-09-28 11:18:04 +0200

    created fb stuff

---
+++ FacebookApps/fb_brands.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Marken</title>
+<link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700|Handlee' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Chela+One' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+
+<body>
+<div class="headline">Das sind unsere Marken</div>
+
+<div id="brand_overview">
+<div class="selcolumn brandO_col_margin" style="margin-left:85px">
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Aber.png" class="brandimages">
+        </div>
+    </div>
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Arma.png" class="brandimages">
+        </div>
+    </div> 
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Bene.png" class="brandimages">
+        </div>
+    </div>
+</div>
+<div class="selcolumn brandO_col_margin">
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Burb.png" class="brandimages">
+        </div>
+    </div>
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Dolc.png" class="brandimages">
+        </div>
+    </div> 
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Hugo.png" class="brandimages">
+        </div>
+    </div>
+</div><div class="selcolumn brandO_col_margin">
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Jack.png" class="brandimages">
+        </div>
+    </div>
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Laco.png" class="brandimages">
+        </div>
+    </div> 
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Napa.png" class="brandimages">
+        </div>
+    </div>
+</div><div class="selcolumn brandO_col_margin">
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Ralp.png" class="brandimages">
+        </div>
+    </div>
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Tomm.png" class="brandimages">
+        </div>
+    </div> 
+    <div class="select brandO_sel_margin">
+        <div class="brandimage_wrapper">
+            <img src="../images/brands/Ving.png" class="brandimages">
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="sell_now clear padding_top_30">
+        <a href="http://www.kleiderkuh.de/sell.php" class="sell_now_button" class="center">
+		<img src="../images/de/buttons/sellNow.png" class="button" />
+        </a>
+</div>
+</body>
+</html>
